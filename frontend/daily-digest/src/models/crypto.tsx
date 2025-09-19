2aae8826833255971c6245e09a8864d4210c4c02
AnaPule <morwetsana.pule@gmail.com>
2025-09-19 08:25:15 +0200

    frontend, news letter set up, css and crypto data retrieval from the database

---
+++ frontend/daily-digest/src/models/crypto.tsx
@@ -0,0 +1,28 @@
+export default interface Crypto {
+    id: number;
+    circulating_supply: number;
+    cmc_rank: number;
+    date_added: Date;
+    infinite_supply: boolean;
+    last_updated: Date;
+    name: string;
+    num_market_pairs: number;
+    self_reported_circulating_supply: number;
+    self_reported_market_cap: number;
+    slug: string;
+    symbol: string;
+    total_supply: number;
+    tags: string[];
+    quote: {
+        USD: {
+            fully_diluted_market_cap: number;
+            last_updated: Date;
+            market_cap: number;
+            market_cap_dominance: number;
+            percent_change_1h: number;
+            price: number;
+            volume_24h: number;
+            volume_change_24h: number;
+        }
+    };
+}
--- frontend/daily-digest/vite.config.ts
+++ frontend/daily-digest/vite.config.ts
@@ -1,11 +1,20 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    tailwindcss(),
+    react()
   ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@models': resolve(__dirname, './src/models'),
+      '@components': resolve(__dirname, './src/components'),
+      '@utils': resolve(__dirname, './src/utils'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+    }
+  }
+
 })
