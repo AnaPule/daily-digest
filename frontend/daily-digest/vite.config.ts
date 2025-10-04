e93624c20a6ef11af17c990407c0def8ec0413c5
AnaPule <morwetsana.pule@gmail.com>
2025-10-04 09:48:19 +0200

    adding output directory for the application

--- frontend/daily-digest/vite.config.ts
+++ frontend/daily-digest/vite.config.ts
@@ -16,6 +16,8 @@ export default defineConfig({
       '@utils': resolve(__dirname, './src/utils'),
       '@hooks': resolve(__dirname, './src/hooks'),
     }
+  },
+  build: {
+    outDir: 'dist'
   }
-
 })
